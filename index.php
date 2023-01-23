d026fcdf75f286366a7bc4e4d75e5ad8e584a9b7
Peter Abah <peterabah.ob@gmail.com>
2023-01-23 14:52:30 +0100

    Add header and footer

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
-<?php require_once "db.php" ?>
+<?php
+require_once "db.php";
+include_once "header.php";
+?>
 
-<!DOCTYPE html>
-<html lang="en">
+<a href="login.php">Login</a>
+<a href="register.php">Register</a>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Members only</title>
-</head>
-
-<body>
-    <h1>Members only</h1>
-    <a href="login.php">Login</a>
-    <a href="register.php">Register</a>
-</body>
-
-</html>
+<?php include_once "footer.php" ?>
