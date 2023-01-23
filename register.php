665d3ceba005bf7269662049c7c5f0f496729792
Peter Abah <peterabah.ob@gmail.com>
2023-01-23 21:47:55 +0100

    Add register page

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+include_once "header.php";
+?>
+
+<form action="auth/register.php" method="post">
+    <label>Username:
+        <input type="text" name="username" maxlength="50" required>
+    </label>
+
+    <label>Password
+        <input type="password" name="password" minlength="8" required>
+    </label>
+
+    <label>Confirm password
+        <input type="password" name="password-confirmation" minlength="8" required>
+    </label>
+
+    <button type="submit">Register</button>
+</form>
+
+<?php if (!empty($_SESSION["error-msg"])): ?>
+    <p>Unable to register. <?= $_SESSION["error-msg"] ?></p>
+<?php endif;
+unset($_SESSION["error-msg"]);
+?>
+
+<?php include_once "footer.php" ?>
