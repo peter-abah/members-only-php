87d20f1a913c58aceb90ce5de3764ed8a8508745
Peter Abah <peterabah.ob@gmail.com>
2023-01-23 21:49:53 +0100

    Move links to header.php

--- header.php
+++ header.php
@@ -1,10 +1,29 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Members only</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Members only</title>
 </head>
+
 <body>
-<h1>Members only</h1>
+    <header>
+        <h1>Members only</h1>
+        <div>
+            <?php if (!empty($_SESSION["username"])): ?>
+                <p><?= $_SESSION["username"] ?></p>
+                <form action="auth/logout.php" method="post">
+                    <button type="submit">Logout</button>
+                </form>
+            <?php else: ?>
+                <a href="login.php">Login</a>
+                <a href="register.php">Register</a>
+            <?php endif ?>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -3,7 +3,5 @@ require_once "db.php";
 include_once "header.php";
 ?>
 
-<a href="login.php">Login</a>
-<a href="register.php">Register</a>
 
 <?php include_once "footer.php" ?>
